5dd65bad1b55204bf9f84d5db1049b1faf5ab483
paulo.moreira <paulo.moreira@ceabs.com.br>
2022-10-03 18:52:12 -0300

    test: Improve unit tests for sample_dag.py

--- runtime/tests/test_sample_dag.py
+++ runtime/tests/test_sample_dag.py
@@ -4,6 +4,7 @@ import unittest
 import pendulum
 from airflow.executors.debug_executor import DebugExecutor
 from airflow.models import DAG, DagBag, TaskInstance
+from airflow.operators.bash import BashOperator
 from airflow.utils.state import DagRunState, TaskInstanceState
 from airflow.utils.types import DagRunType
 
@@ -17,6 +18,7 @@ class SampleDagTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.dag_bag = DagBag()
+        self._clear_dag(self.dag_id)
 
     def _getDag(self) -> DAG:
         return self.dag_bag.get_dag(dag_id=self.dag_id)
@@ -41,18 +43,15 @@ class SampleDagTest(unittest.TestCase):
         data_interval_start = pendulum.datetime(2022, 9, 14, tz="UTC")
         data_interval_end = data_interval_start + datetime.timedelta(days=1)
 
-        try:
-            dagrun = dag.create_dagrun(
-                state=DagRunState.SUCCESS,
-                execution_date=data_interval_start,
-                data_interval=(data_interval_start, data_interval_end),
-                start_date=data_interval_end,
-                run_type=DagRunType.MANUAL,
-                run_id=f'test_{pendulum.now().isoformat()}',
-                external_trigger=True
-            )
-        except:
-            print("dag run already exists")
+        dagrun = dag.create_dagrun(
+            state=DagRunState.RUNNING,
+            execution_date=data_interval_start,
+            data_interval=(data_interval_start, data_interval_end),
+            start_date=data_interval_end,
+            run_type=DagRunType.MANUAL,
+            run_id=f'test_{pendulum.now().isoformat()}',
+            external_trigger=True
+        )
 
         task = dag.get_task(task_id="Begin_execution")
         ti = TaskInstance(task, data_interval_start)
@@ -67,3 +66,26 @@ class SampleDagTest(unittest.TestCase):
         ti.run(ignore_ti_state=True)
         assert ti.state == TaskInstanceState.SUCCESS
         # Assert something related to tasks results.
+
+    def _clear_dag(self, dag_id):
+        """
+        Delete all DB records related to the specified DAG.
+        seealso: https://airflow.apache.org/docs/apache-airflow/stable/cli-and-env-variables-ref.html#delete_repeat1
+        :param dag_id:
+        :return: None
+        """
+        from airflow.utils.context import Context
+        dag = DAG(
+            "clear_dag",
+            schedule=None,
+            start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+            catchup=False,
+        )
+
+        parameterized_task = BashOperator(
+            task_id="clear_dag_task",
+            bash_command=f"airflow dags delete -y {dag_id}",
+            dag=dag,
+        )
+        context = Context()
+        parameterized_task.execute(context=context)
